284ee9bbc8c5b1715d20f5c93bec6233d3b33e79
YammD123 <siamqueen52@gmail.com>
2025-04-04 02:39:26 +0700

    add pagination category and delete category

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { PaginationDto } from './dto/pagination.dto';
 
 @Controller('category')
 export class CategoryController {
@@ -12,7 +13,12 @@ export class CategoryController {
     }
 
     @Get()
-    async getAllCategory(){
-        return this.categoryService.getAllCategory()
+    async getAllCategory(@Query() paginationDto:PaginationDto){
+        return this.categoryService.getAllCategory(paginationDto)
+    }
+
+    @Delete(':id')
+    async deleteCategory(@Param('id') id:string){
+        return this.categoryService.deleteCategory(id)
     }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,33 +1,55 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { PaginationDto } from './dto/pagination.dto';
 
 @Injectable()
 export class CategoryService {
-    constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-    async createCategory(createCategoryDto:CreateCategoryDto){
-        try {
-            const createCategory = await this.prisma.category.create({
-                data:{
-                    ...createCategoryDto
-                }
-            })
-            return {message:"Category berhasil di buat",data:createCategory};
-        } catch (error) {
-            throw error
-        }
+  async createCategory(createCategoryDto: CreateCategoryDto) {
+    try {
+      const createCategory = await this.prisma.category.create({
+        data: {
+          ...createCategoryDto,
+        },
+      });
+      return { message: 'Category berhasil di buat', data: createCategory };
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getAllCategory(){
-        try {
-            const getAllCategory = await this.prisma.category.findMany()
-            if(getAllCategory.length === 0){
-                throw new HttpException('Category belum ada',HttpStatus.NOT_FOUND)
-            }
-            return {message:"Category berhasil ditemukan",data:getAllCategory};
-        } catch (error) {
-            throw error
-        }
+  async getAllCategory(paginationDto: PaginationDto) {
+    try {
+      const getAllCategory = await this.prisma.category.findMany({
+        take:Number (paginationDto.limit),
+        skip: Number (paginationDto.page - 1) * paginationDto.limit,
+      });
+      if (getAllCategory.length === 0) {
+        throw new HttpException('Category belum ada', HttpStatus.NOT_FOUND);
+      }
+      const totalCategory = await this.prisma.category.count();
+      const totalPage = Math.ceil(totalCategory / paginationDto.limit);
+      return { message: 'Category berhasil ditemukan',totalCategory,totalPage,totalData:paginationDto.limit, data: getAllCategory };
+    } catch (error) {
+      throw error;
     }
+  }
+
+  async deleteCategory(id: string) {
+    try {
+      const deleteCategory = await this.prisma.category.delete({
+        where: {
+          id: id,
+        }
+      })
+      if(!deleteCategory){
+        throw new HttpException('Category gagal dihapus',HttpStatus.BAD_REQUEST)
+      }
+      return { message: 'Category berhasil dihapus', data: deleteCategory };
+    } catch (error) {
+      throw error
+    }
+  }
 }
